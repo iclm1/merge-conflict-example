9f82b4f5f0307df612dc378df2c2e6f103c04455
iclm1 <101156773+iclm1@users.noreply.github.com>
2024-04-11 23:02:38 -0300

    novo test

--- teste1.py
+++ teste1.py
@@ -1,12 +1,12 @@
 class Pessoa:
-    def __init__(self, nome, idadex, profissaox):
-        self.nome = nome
+    def __init__(self, nomex, idadex, profissaox):
+        self.nome = nomex
         self.idade = idadex
         self.profissao = profissaox
 
 
 usuario1 = Pessoa("Leo", 20, "estudante")
 usuario2 = Pessoa("João", 32, "Engenheiro")
-print(usuario1.idade)
-
+print(usuario2.idade)
+print(usuario1.nome)
 
